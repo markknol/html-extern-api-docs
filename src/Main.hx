e6952703bd237b2d07640a5019280566ca8a21cb
Mark <mark@mediamonks.com>
2016-12-05 17:22:13 +0100

    cleanup

--- src/Main.hx
+++ src/Main.hx
@@ -9,168 +9,333 @@ import sys.io.File;
 using StringTools;
 
 /**
- * ...
- * @author Mark Knol
+	Haxe HTML extern API documentation MDN wiki processor factory macroless bean visitor-facade application aware injection decorator layer.
+
+	Example MDN url <https://developer.mozilla.org/en-US/docs/Web/API/window>
+	Can be loaded "raw" like this, and a certain section <https://developer.mozilla.org/en-US/docs/Web/API/window?raw&section=Properties>
+
+	What I do is load the file content of "js.html.Window.hx" of the in-directory, find its properties/methods, search in the MDN document for same properties/methods, search its documentation, put it back in the Haxe file, store in out-directory. 
+	I also get+inject the summary of the MDN page.
+	
+	@see MDN document parameters: https://developer.mozilla.org/en-US/docs/MDN/Contribute/Tools/Document_parameters
+ 
+	@author Mark Knol
  */
-class Main 
+class Main
 {
-	static function main() 
-	{
-		new Main();
+	// base out folder
+	static inline var OUT_FOLDER = "out/";
+	
+	// base in folder
+	static inline var IN_FOLDER = "html/";
+	
+	// all scraped data will be stored here. When running for second time it will use data from disk
+	static inline var DATA_FOLDER = "data/";
+	
+	static inline var MDN_URL = "https://developer.mozilla.org/en-US/docs/Web/API/";
+	
+	
+	static function main() new Main();
+
+
+	// store file temporary here, to allow multiple edits
+	private var processedHaxeFile:String;
+	
+	// stats of collected data. 
+	private var stats = {
+		classes: {
+			total: 0,
+			replaced: 0,
+		},
+		methods: {
+			total: 0,
+			replaced: 0,
+		}, 
+		properties: {
+			total: 0,
+			replaced: 0,
+		},
+		summaries: {
+			total: 0,
+			replaced: 0,
+		}
 	}
-	
-	public var totalClasses:Int = 0;
-	public var replacedClasses:Int = 0;
-	public var totalMethods:Int = 0;
-	public var totalProperties:Int = 0;
-	public var replacedMethods:Int = 0;
-	public var replacedProperties:Int = 0;
-	
-	var newClassData:String;
-	
+
 	public function new()
 	{
-		for (file in FileSystem.readDirectory("out/"))
+		// create cache data folder
+		FileSystem.createDirectory(DATA_FOLDER);
+		
+		
+		var basePack = "js.html";
+		
+		// process base package
+		process(IN_FOLDER, OUT_FOLDER, basePack);
+		
+		// process sub packages
+		for (pack in ["audio", "idb", "rtc", "svg", "webgl"])
 		{
-			FileSystem.deleteFile("out/" + file);
+			process('${IN_FOLDER}${pack}/', '${OUT_FOLDER}${pack}/', '$basePack.$pack');
 		}
 		
-		FileSystem.createDirectory("out");
-		var i = 0;
-		for (fileName in FileSystem.readDirectory("html/"))
+		logStats();
+	}
+	
+	private function process(inFolder:String, outFolder:String, pack:String)
+	{
+		trace('processing $pack ..');
+		
+		// clean output
+		FileSystem.createDirectory(outFolder);
+		for (file in FileSystem.readDirectory(outFolder))
+			if (!FileSystem.isDirectory(outFolder + file)) 
+				FileSystem.deleteFile(outFolder + file);
+		
+		// read through all extern files
+		for (fileName in FileSystem.readDirectory(inFolder))
 		{
-			//if (i > 10) return;
-			if (!FileSystem.isDirectory("html/" + fileName))
+			if (!FileSystem.isDirectory(inFolder + fileName))
 			{
-				totalClasses++;
+				stats.classes.total++;
 				var thing = fileName.split(".").shift();
-				newClassData = File.getContent('html/${thing}.hx');
-				
-				if (newClassData.indexOf("\n@:native(") != -1) {
-					thing = newClassData.split("\n@:native(\"").pop().split("\")").shift();
-				}
+				processedHaxeFile = File.getContent('$inFolder${thing}.hx');
 
-				scrape(thing, Properties);
-				scrape(thing, Methods);
-				scrapeSummary(thing, Summary);
-					
-				File.saveContent('out/$thing.hx', newClassData);
-				i ++;
-			}
-			
-			if (i % 10 == 0)
-			{
-				trace("avg classes : " + (replacedClasses / totalClasses));
-				trace("avg methods : " + (replacedMethods / totalMethods));
-				trace("avg properties : " + (replacedProperties / totalProperties));
+				// extract native type name
+				var nativeMetaSearch = "\n@:native(\"";
+				if (processedHaxeFile.indexOf(nativeMetaSearch) != -1) thing = processedHaxeFile.split(nativeMetaSearch).pop().split("\")").shift();
+				
+				// process properties
+				var foundProps = processThing(thing, Properties, pack);
+				
+				// process methods
+				var foundMethods = processThing(thing, Methods, pack);
+				
+				// process summary
+				var foundSummary = processThing(thing, Summary, pack);
+
+				if (foundProps || foundMethods || foundSummary) stats.classes.replaced ++;
+				
+				// finally, save in out folder, no matter if something actually is replaced
+				File.saveContent('$outFolder$fileName', processedHaxeFile);
 			}
 		}
 	}
 	
-	public function scrapeSummary(thing:String, type:MSDNType)
+	private function getMDNData(thing:String, type:MSDNType)
 	{
-		var msdnData = try
-			Http.requestUrl('https://developer.mozilla.org/en-US/docs/Web/API/$thing?raw&$type')
-		catch (e:Dynamic)
-			null;
-			
-		
-		if (msdnData == null || msdnData.length <= 2)
+		// get mdn url. 
+		var url = switch(type)
 		{
-			trace(thing, type, "failed");
-			return false;
+			case Methods, Properties:
+				'$MDN_URL$thing?raw&section=$type';
+				
+			case Summary:
+				'$MDN_URL$thing?raw&$type';
 		}
-		trace(thing, type, "success");
 		
-		msdnData = cleanDoc(msdnData);
-		
-		var query = "package js.html;";
-		newClassData = newClassData.replace(query, query + "\n\n/**\n\t" + msdnData + "\n**/");
-		return true;
-	}
-	
-	public function scrape(thing:String, type:MSDNType)
-	{
-		var msdnData = try
-			Http.requestUrl('https://developer.mozilla.org/en-US/docs/Web/API/$thing?raw&section=$type')
-		catch(e:Dynamic)
-			//try
-				//Http.requestUrl('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/$thing?raw&section=$type')
-			//catch(e:Dynamic)
-				//try
-					//Http.requestUrl('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/$thing?raw&section=$type')
-				//catch(e:Dynamic)
+		// request data from disk, otherwise download it
+		var data = 
+			if (FileSystem.exists('$DATA_FOLDER$thing-$type')) 
+				File.getContent('$DATA_FOLDER$thing-$type')
+			else 
+				try
+					Http.requestUrl(url)
+				catch (e:Dynamic)
 					null;
-		
-		if (msdnData == null || msdnData.length <= 5)
+			
+		if (data == null || data.length <= 5)
 		{
-			trace(thing, type, "failed");
-			return false;
+			// store a empty file
+			//trace(thing, type, "failed");
+			File.saveContent('$DATA_FOLDER$thing-$type', "fail");
+			return null;
 		}
 		
-		replacedClasses++;
-		trace(thing, type, "success");
+		// store on disk
+		File.saveContent('$DATA_FOLDER$thing-$type', data);
+		return data;
+	}
+
+	private function processThing(thing:String, type:MSDNType, pack:String)
+	{
+		var mdnData = getMDNData(thing, type);
+		if (mdnData == null) return false;
+		//trace(thing, type, "success");
 		
-		switch(type)
+		inline function getAsString(v:String) return (v != null && v.length > 0) ? v : "";
+		
+		switch (type)
 		{
 			case Methods:
-				var classData = newClassData;
-				var regexp = ~/function (.+?)(\()/ig;
 				
-				while (regexp.match(classData))
+				var regexp = ~/(\/\*\* (.+?) \*\/\n\t)?(function (.+?)(\())/ig;
+				// replace extern file content with replaced data
+				processedHaxeFile = regexp.map(processedHaxeFile, function(regexp) 
 				{
-					totalMethods++;
-					var property = regexp.matched(1);
-					var propertySearch = '<dt>{{domxref("$thing.$property(';
-					if (msdnData.indexOf(propertySearch) > -1)
-					{
-						var splitted = msdnData.split(propertySearch).pop().split("</dd>").shift();
-						var doc = cleanDoc(splitted.split('<dd>').pop());
-						//trace(property, doc);
-						replacedMethods++;
-						
-						var orig = 'function $property' + regexp.matched(2);
-						newClassData = newClassData.replace(orig, "/**\n\t\t" + doc + "\n\t**/\n\t" + orig);
+					stats.methods.total ++;
+					var methodName = regexp.matched(4);
+					
+					function search(query:String) {
+						if (mdnData.indexOf(query) > -1)
+						{
+							// get doc of this method. seach for data in next dd-element
+							var doc = cleanDoc(mdnData.split(query).pop().split('</dd>').shift().split('<dd>').pop());
+							
+							stats.methods.replaced ++;
+
+							// get api docs of extern file method, if any. just prepend it for now.
+							var origDoc = getAsString(regexp.matched(2));
+							origDoc = (origDoc.length > 0) ? '\n\t\t$origDoc' : '';
+							
+							return "/**\n\t\t" + doc  + origDoc + "\n\t**/\n\t" + regexp.matched(3);
+						}
+						return null;
 					}
-					classData = regexp.matchedRight();
-				}
+					
+					// sometimes definition looks like this
+					var result = search('<dt>{{domxref("$thing.$methodName(');
+					if (result != null) return result;
+					
+					// sometimes definition looks like this
+					var result = search('<dt>$methodName(');
+					if (result != null) return result;
+					
+					// sometimes definition looks like this
+					var result = search('<dt><code>$methodName(');
+					if (result != null) return result;
+					
+					return getAsString(regexp.matched(1)) + regexp.matched(3);
+				});
 				
 				
 			case Properties:
-				var classData = newClassData;
-				var regexp = ~/var (.+?)(\(|\s)/g;
-				
-				while (regexp.match(classData))
+				var regexp = ~/(\/\*\* (.+?) \*\/\n\t)?(var (.+?)(\(|\s))/g;
+				// replace extern file content with replaced data
+				processedHaxeFile = regexp.map(processedHaxeFile, function(regexp) 
 				{
-					totalProperties++;
-					var property = regexp.matched(1);
-					var propertySearch = '<dt>{{domxref("$thing.$property")}}';
-					if (msdnData.indexOf(propertySearch) > -1)
-					{
-						var splitted = msdnData.split(propertySearch).pop().split("</dd>").shift();
-						var doc = cleanDoc(splitted.split('<dd>').pop());
-						
-						//trace(property, doc);
-						var orig = 'var $property' + regexp.matched(2);
-						newClassData = newClassData.replace(orig, "/**\n\t\t" + doc + "\n\t**/\n\t" + orig);
-						replacedProperties++;
+					stats.properties.total ++;
+					var property = regexp.matched(4);
+					
+					function search(query:String) {
+						if (mdnData.indexOf(query) > -1)
+						{
+							// get doc of this property. seach for data in next dd-element
+							var doc = cleanDoc(mdnData.split(query).pop().split('</dd>').shift().split('<dd>').pop());
+							
+							stats.properties.replaced ++;
+							
+							// get api docs of extern file property, if any. just prepend it for now.
+							var origDoc = getAsString(regexp.matched(2));
+							origDoc = (origDoc.length > 0) ? '\n\t\t$origDoc' : '';
+							
+							return '/**\n\t\t$doc$origDoc\n\t**/\n\t' + regexp.matched(3);
+						}
+						return null;
 					}
-					classData = regexp.matchedRight();
+					
+					// sometimes definition looks like this
+					var result = search('<dt>{{domxref("$thing.$property")}}');
+					if (result != null) return result;
+					
+					// sometimes definition looks like this
+					var result = search('<dt>$property<');
+					if (result != null) return result;
+					
+					// sometimes definition looks like this
+					var result = search('<dt><code>$property<');
+					if (result != null) return result;
+					
+					return getAsString(regexp.matched(1)) + regexp.matched(3);
+				});
+
+				
+			case Summary:
+				mdnData = cleanDoc(mdnData);
+				stats.summaries.total ++; 
+				
+				if (mdnData.length > 2) 
+				{
+					stats.summaries.replaced ++;
+					
+					// put summary after package definition
+					var query = 'package $pack;';
+					var credits = '@see <$MDN_URL$thing>';
+					// replace extern file content with replaced data
+					processedHaxeFile = processedHaxeFile.replace(query, '$query\n\n/**\n\t$mdnData \n\n\t$credits \n**/');
 				}
-			
-			default:
 		}
+		
 		return true;
 	}
-	
-	function cleanDoc(value:String) 
+
+	private function cleanDoc(value:String)
 	{
-		value = ~/<(?:.|\n)*?>/gm.replace(value, '');
-		value = ~/{{(.+?)\("(.+?)"(.+?)?}}/gm.replace(value, '`$2`');
-		return value;
+		// when there is a table, we keep all html tags, because markdown/html doesnt mix well
+		if (value.indexOf("<table")>-1)
+		{
+			// when there is a table, keep all relevant html
+			value = stripTags(value, ["pre", "code", "table", "tr", "td", "br"]);
+
+			// remove wiki markup. dont mix html and markdown
+			value = ~/{{(.+?)\("(.+?)"(.+?)?}}/gm.replace(value, '<code>$2</code>');
+		}
+		else
+		{
+			// if there is no table, convert stuff to markdown
+			value = ~/<\/?(code)>/ig.replace(value, "`");
+			value = ~/<\/?(br)\/?>/ig.replace(value, "\n");
+			
+			// remove all other html tags
+			value = ~/<(?:.|\n)*?>/gm.replace(value, '');
+
+			// remove wiki markup, replace with code
+			value = ~/{{(.+?)\(("|')(.+?)("|')(.+?)?}}/gm.replace(value, '`$3`');
+		}
+		
+		// remove some html-entities. 
+		value = value.replace("&lt;", "").replace("&gt;", "");
+		
+		// return indented
+		return value.split("\n").join("\n\t\t");
+	}
+
+	inline function stripTags(value:String, keepTags:Array<String>)
+	{
+		var tags = [];
+		for (tag in keepTags)
+		{
+			tags.push(tag);
+			tags.push("/" + tag);
+		}
+
+		if (tags.length == 0)
+		{
+			return ~/<(\s*\/?)[^>]+>/g.replace(value, "");
+		}
+		else
+		{
+			var ereg = new EReg("<(?!(" + tags.join("|") + ")\\s*\\/?)[^>]+>", "g" );
+			return ereg.replace(value, '' );
+		}
+	}
+	
+	private function logStat(stat:{total:Int, replaced:Int})
+	{
+		return Std.int(stat.replaced / stat.total * 100) + '%  (total=${stat.total})';
+	}
+	
+	private function logStats() 
+	{
+		trace('-- STATS --');
+		trace('classes found: ${logStat(stats.classes)}');
+		
+		trace('methods added: ${logStat(stats.methods)}');
+		trace('properties added: ${logStat(stats.properties)}');
+		trace('summaries added: ${logStat(stats.summaries)}');
 	}
 }
 
+// capitals of values are different here on purpose
 @:enum abstract MSDNType(String) to String
 {
 	var Methods = "Methods";
